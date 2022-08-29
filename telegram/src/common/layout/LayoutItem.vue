593191e7e74d5b9b430cb29dbf8eb32c6f302da3
qing-m <1874373481@qq.com>
2022-08-29 10:00:20 +0800

    fix

--- telegram/src/common/layout/LayoutItem.vue
+++ telegram/src/common/layout/LayoutItem.vue
@@ -1,11 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import NavBar from "@/common/layout/components/NavBar/NavBar.vue";
+import AppMain from "@/common/layout/components/AppMain/AppMain.vue";
+import FooterItem from "@/common/layout/components/FooterItem/FooterItem.vue";
+</script>
 
 <template>
-  <div>
-    <div>nav</div>
-    <router-view></router-view>
-    <div>footer</div>
+  <div class="layout_container">
+    <nav-bar></nav-bar>
+    <app-main>
+      <router-view></router-view>
+    </app-main>
+    <footer-item></footer-item>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.layout_container {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+</style>
