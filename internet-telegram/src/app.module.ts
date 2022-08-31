6279a0e7c1b01fd4786a18ba8ce9a92561e48891
qing-m <1874373481@qq.com>
2022-08-31 16:45:51 +0800

    fix

--- internet-telegram/src/app.module.ts
+++ internet-telegram/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 
 @Module({
@@ -17,6 +16,4 @@ import { AppController } from './app.controller';
   ],
   controllers: [AppController],
 })
-export class AppModule {
-  constructor(private readonly connection: Connection) {}
-}
+export class AppModule {}
